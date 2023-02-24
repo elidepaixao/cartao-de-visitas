809e8f40a0f310b221e4f8f3259c5c69ad8fcd34
Elide Paixao <elidepaixao@gmail.com>
2023-02-24 20:40:06 -0300

    profile img

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 function toggleMode() {
+  //usar a tag html
   const html = document.documentElement
+  //substituir o background
   html.classList.toggle("light")
 
+  //usar a tag img
   const img = document.querySelector("#profile img")
 
+  //substituir a imagem
    if (html.classList.contains("light")) {
-     img.setAttribute("src", "./assets/avatar.png")
-   } else {
+    //se tiver no light mode, adicionar a imagem light
      img.setAttribute("src", "./assets/avatar-light.png")
+   } else {
+    //dark mode, manter a imagem normal
+     img.setAttribute("src", "./assets/avatar.png")
    }
 }
